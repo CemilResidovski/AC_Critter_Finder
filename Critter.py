664d2a7d95a35c98c43e4af804d7b407e12a20f7
dpollozhani <38571830+dpollozhani@users.noreply.github.com>
2020-04-02 16:07:43 +0200

    Add files via upload

---
+++ Critter.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import Utils
+import calendar
+
+month_to_num = {v.lower(): k for k,v in enumerate(calendar.month_abbr) if k > 0}
+
+'''TODO: load files here or in main?'''
+df_fish = pd.read_excel('fish.xlsx')
+df_bugs = pd.read_excel('bugs.xlsx')
+
+class Critter():
+
+    def __init__(self, df, ctype):
+        assert isinstance(df, pd.DataFrame), "df must be pandas dataframe with appropriate critter info!"
+        self.df = df
+        assert isinstance(ctype, str) and ctype.lower() in ['fish', 'bug'], "ctype must be \'fish\' or \'bug\'!"
+        self.ctype = ctype
+
+    def new(self):
+        '''Return critters that are available current month but not previous month'''
+        current_month_text = Utils.curr_month()
+        prev_month_text = Utils.prev_month()
+        
+        filter_this_month = (self.df['Months'] == current_month_text) & (self.df['isMonth'])
+        filter_prev_month = (self.df['Months'] == prev_month_text) & (self.df['isMonth'] == False)
+        
+        critter_this_month = set(self.df[filter_this_month][self.ctype].values) #critter that are available current month
+        critter_prev_month = set(self.df[filter_prev_month][self.ctype].values) #critter that were not available previous month
+        
+        result = critter_this_month.intersection(critter_prev_month) #the intersection of above two sets
+        
+        return result
+    
+    def expiring(self):
+        '''Return critters that are available current month but not next month'''
+        current_month_text = Utils.curr_month()
+        next_month_text = Utils.next_month()
+        
+        filter_this_month = (self.df['Months'] == current_month_text) & (self.df['isMonth'])
+        filter_next_month = (self.df['Months'] == next_month_text) & (self.df['isMonth'] == False)
+        
+        critter_this_month = set(self.df[filter_this_month][self.ctype].values) #critter that are available current month
+        critter_next_month = set(self.df[filter_next_month][self.ctype].values) #critter that are not available next month
+        
+        result = critter_this_month.intersection(critter_next_month) #the intersection of above two sets
+        
+        return result
+
+    
+    def get_info(self, critter):
+        '''Return info on when and where to find specific critter (name of a specific critter, e.g. 'Stringfish')'''
+
+        assert critter in self.df[self.ctype].values, f"{critter} not found, make sure spelling is correct!"
+
+        this_df = self.df[self.df['isMonth'] & self.df['isTime'] & (self.df[self.ctype] == critter)]
+        this_df = this_df[['location', 'Months', 'Times']]
+        this_df['Times'] = this_df['Times'].astype(str)
+        
+        group = this_df.groupby(['location', 'Months'])
+        
+        result = group['Times'].apply(','.join).reset_index() #available times per (available) month and location for chosen critter
+        
+        #sorting on months
+        result['MonthsNum'] = result['Months'].map(month_to_num)
+        result = result.sort_values(by=['MonthsNum'])
+        result.drop('MonthsNum', axis=1, inplace=True)
+        
+        return result
+    
+
+#fish, bug = Critter(df_fish, 'fish'), Critter(df_bugs, 'bug')
+
+#print(fish.new())
+#print(bug.new())
+#print(fish.expiring())
+#print(bug.expiring())
+#print(fish.get_info('Stringfish'))
+#print(bug.get_info('Common Butterfly'))
